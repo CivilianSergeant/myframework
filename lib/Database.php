d702b83c373020c4a92a63405122c9696a88749f
muhimel <mu.himel@gmail.com>
2017-04-27 11:21:41 +0600

    Update Database.php

--- lib/Database.php
+++ lib/Database.php
@@ -24,6 +24,17 @@ class Database {
     private function __construct() 
     {
         if(empty(self::$conn)){
+            
+            $databaseConfig = Config::get('database');
+            $host   = $databaseConfig['host'];
+            $driver = $databaseConfig['driver'];
+            $user   = $databaseConfig['user'];
+            $pass   = $databaseConfig['pass'];
+            $dbName = $databaseConfig['dbname']; 
+            if(empty($databaseConfig) || empty($driver) || empty($host) || empty($user) || empty($dbName)){
+                throw new Exception("Check Database configuration at config.php",500);
+            }
+            
             self::$conn = new \PDO("mysql:host=localhost;dbname=test","root","");
         }
     }
